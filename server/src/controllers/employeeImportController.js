37a4c7669aba1614ab4a8916b2cc3d2796621d06
aditya-paul-2907 <pauladitya2907@gmail.com>
2026-09-22 17:07:56 +0530

    Expose temporary employee import debug error

--- server/src/controllers/employeeImportController.js
+++ server/src/controllers/employeeImportController.js
@@ -65,7 +65,9 @@ const previewImport = async (req, res) => {
         console.error("EMPLOYEE IMPORT PREVIEW ERROR:", error);
         return res.status(400).json({
             success: false,
-            message: "Unable to parse the employee import workbook."
+            message: "Unable to parse the employee import workbook.",
+            debugError: error.message,
+            debugCode: error.code || null
         });
     }
 };
